bd4d58e5fea5c2627339579edc1d756d926ac415
Matheus Barcellos <mathbar4@hotmail.com>
2023-11-30 20:06:40 -0300

    refactored day 17

--- 2022/python/day17.py
+++ 2022/python/day17.py
@@ -1,12 +1,14 @@
 import itertools as it
 from collections import defaultdict
 
+from .common.common import read_file_to_string
+
 test_pattern = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
 test_other = ">><<"
 
-Coord = (int, int)
+Coord = tuple[int, int]
 
-rocks = (
+ROCKS = (
     [(0, 0), (1, 0), (2, 0), (3, 0)],  # minus
     [(1, 0), (0, 1), (1, 1), (2, 1), (1, 2)],  # plus
     [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)],  # inverted l
@@ -14,37 +16,42 @@ rocks = (
     [(0, 0), (0, 1), (1, 0), (1, 1)],  # square
 )
 
-
-def move(rock: list[Coord], dx: int, dy: int):
-    return [(x + dx, y + dy) for (x, y) in rock]
-
-
-def hits_wall(rock, dx):
-    for p in rock:
-        if not (0 <= p[0] + dx <= 6):
-            return True
-    return False
-
-
-def hits_rock(rock, superset, dx, dy):
-    for (x, y) in rock:
-        if (x + dx, y + dy) in superset:
-            return True
-    return False
-
-
-def prune(G, top):
-    for p in [p for p in G if p[1] < top - 100]:
-        G.remove(p)
-
-
-def main() -> None:
-    with open("day17.txt") as fp:
-        pattern = fp.read().strip()
+def part1() -> int:
+    pattern = read_file_to_string(__file__)
 
     count = 0
-    G = set([(i, 0) for i in range(7)])
     top = 0
+    G = set([(i, 0) for i in range(7)])
+
+    rock_iterator = it.cycle(ROCKS)
+    jet_dir = it.cycle([-1 if ch == "<" else 1 for ch in pattern])
+
+    while count < 2022:
+        rock = [(x + 2, y + 4 + top) for (x, y) in next(rock_iterator)]
+
+        while True:
+            # moving sideways
+            dir = next(jet_dir)
+            if not hits_wall(rock, dir) and not hits_rock(rock, G, dir, 0):
+                rock = move(rock, dir, 0)
+
+            # moving down
+            if hits_rock(rock, G, 0, -1):
+                break
+            rock = move(rock, 0, -1)
+
+        G.update([(x, y) for (x, y) in rock])
+        top = max([p[1] for p in rock] + [top])
+        prune(G, top)
+        count += 1
+    return top
+
+def part2() -> int:
+    pattern = read_file_to_string(__file__)
+
+    count = 0
+    top = 0
+    G = set([(i, 0) for i in range(7)])
     target = 1_000_000_000_000
 
     # cycle measurements
@@ -54,7 +61,7 @@ def main() -> None:
     cycle_amount = None
     tgtidx = -1
 
-    rock_iterator = it.cycle(rocks)
+    rock_iterator = it.cycle(ROCKS)
     jet_dir = it.cycle([-1 if ch == "<" else 1 for ch in pattern])
 
     while count < 1_000_000_000_000:
@@ -74,16 +81,10 @@ def main() -> None:
         prune(G, top)
         count += 1
 
-        # part 1
-        if count == 2022:
-            print("Answer 1:", top)
-
         # part 2 after finding divisor
-        if count == tgtidx:
+        if count == tgtidx and divisor:
             mod = top - (initial_height + ((count // divisor) - 1) * cycle_amount)
-            part2 = initial_height + ((target // divisor) - 1) * cycle_amount + mod
-            print("Answer 2:", part2)
-            break
+            return initial_height + ((target // divisor) - 1) * cycle_amount + mod
 
         # skip tracking after finding divisor
         if divisor is not None:
@@ -110,14 +111,34 @@ def main() -> None:
 
                 # run for modulus more cycles
                 tgtidx = count + (target % divisor)
-                # delete tracker
-                tracker = None
                 break
 
             elif len(tracker[i]) > 3 and tracker[i][-1][1] != tracker[i][-2][1]:
                 del tracker[i]
-    return
 
 
-if __name__ == "__main__":
-    main()
+    return top
+
+def move(rock: list[Coord], dx: int, dy: int):
+    return [(x + dx, y + dy) for (x, y) in rock]
+
+
+def hits_wall(rock, dx):
+    for p in rock:
+        if not (0 <= p[0] + dx <= 6):
+            return True
+    return False
+
+
+def hits_rock(rock, superset, dx, dy):
+    for (x, y) in rock:
+        if (x + dx, y + dy) in superset:
+            return True
+    return False
+
+
+def prune(G, top):
+    for p in [p for p in G if p[1] < top - 100]:
+        G.remove(p)
+
+
