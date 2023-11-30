6400b046aa5c6205deb2a6b48bf132b523f34b0e
Matheus Barcellos <mathbar4@hotmail.com>
2023-11-30 18:45:49 -0300

    chore: add test files for every day

---
+++ 2022/python/tests/day17_test.py
@@ -0,0 +1,9 @@
+from ..day17 import part1, part2
+
+
+def test_part1():
+    assert part1() == 3157
+
+
+def test_part2():
+    assert part2() == 1581449275319
---
+++ 2022/python/tests/day19_test.py
@@ -0,0 +1,9 @@
+from ..day19 import part1, part2
+
+
+def test_part1():
+    assert part1() == 1589
+
+
+def test_part2():
+    assert part2() == 29348
