660e7fb6fe96801a9adb9cb4e7c192a486c3338e
Matheus Barcellos <mathbar4@hotmail.com>
2023-11-30 18:20:27 -0300

    go day 2

--- 2022/go/day2.go
+++ 2022/go/day2.go
@@ -41,6 +41,40 @@ func points_for_result(left, right string) (int, error) {
 	return points, nil
 }
 
+func pick_play_from_result(their_play, result string) (string, error) {
+	var points int
+	switch their_play {
+	case "A":
+		if right == "X" {
+			points = 3
+		} else if right == "Y" {
+			points = 6
+		} else {
+			points = 0
+		}
+	case "B":
+		if right == "X" {
+			points = 0
+		} else if right == "Y" {
+			points = 3
+		} else {
+			points = 6
+		}
+	case "C":
+		if right == "X" {
+			points = 6
+		} else if right == "Y" {
+			points = 0
+		} else {
+			points = 3
+		}
+	default:
+		return -1, errors.New("their move is invalid")
+	}
+
+	return points, nil
+}
+
 func points_for_play(play string) (int, error) {
 	var points int
 	switch play {
@@ -57,7 +91,31 @@ func points_for_play(play string) (int, error) {
 }
 
 func part1(file string) (int, error) {
-	points := 0
+	var points int
+	for _, line := range strings.Split(file, "\n") {
+		plays := strings.Split(line, " ")
+		if len(plays) > 2 {
+			return -1, errors.New("invalid line")
+		}
+		theirs, mine := plays[0], plays[1]
+
+		play_points, err := points_for_play(mine)
+		if err != nil {
+			panic(err)
+		}
+		result_points, err := points_for_result(theirs, mine)
+		if err != nil {
+			panic(err)
+		}
+
+		points += play_points + result_points
+	}
+
+	return points, nil
+}
+
+func part2(file string) (int, error) {
+	var points int
 	for _, line := range strings.Split(file, "\n") {
 		plays := strings.Split(line, " ")
 		if len(plays) > 2 {
