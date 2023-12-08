7fd40bd55eee9c064c0d2150bd247b0043053cb0
Matheus Barcellos <mathbar4@hotmail.com>
2023-12-08 07:47:15 -0300

    half of day 8

---
+++ 2023/python/day08.py
@@ -0,0 +1,50 @@
+from .common.common import read_file_to_string
+import itertools as it
+
+
+def part1() -> int:
+    desc = read_file_to_string(__file__)
+    instructions, net = parse_network(desc)
+    turns = simulate(instructions, net)
+    return turns
+
+
+def part2() -> int:
+    return 1
+
+
+Network = dict[str, list[str]]
+
+
+def parse_network(desc: str) -> tuple[it.cycle, Network]:
+    instructions, nodes_str = desc.split("\n\n")
+
+    instructions_as_indices = it.cycle(0 if i == "L" else 1 for i in instructions)
+
+    # do not do this at home
+    key_vals = [line.split(" = ") for line in nodes_str.splitlines()]
+    net = {key: string_val[1:-1].split(", ") for key, string_val in key_vals}
+
+    return instructions_as_indices, net
+
+
+def simulate(instructions: it.cycle, network: Network) -> int:
+    pos: str = "AAA"
+    jumps = 0
+
+    while pos != "ZZZ":
+        pos = network[pos][next(instructions)]
+        jumps += 1
+
+    return jumps
+
+
+def simulate_pt2(instructions: it.cycle, network: Network) -> int:
+    start_poss = [node for node in network if node.endswith("A")]
+    jumps = 0
+
+    while pos != "ZZZ":
+        pos = network[pos][next(instructions)]
+        jumps += 1
+
+    return jumps
