d10e0087a6e3633996f8b20f7b6b8dcb58de1713
Matheus Barcellos <mathbar4@hotmail.com>
2023-12-21 08:08:16 -0300

    wip: day 17

---
+++ 2023/python/tests/day17_test.py
@@ -0,0 +1,28 @@
+from ..day17 import Board, part1, part2
+
+test_input = """2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533"""
+
+
+def test_parse():
+    board = Board(test_input)
+    assert board.simulate() == 102
+
+
+def test_part1():
+    assert part1() == 1
+
+
+def test_part2():
+    assert part2() == 1
