102972cfbc53f28cfd67feccfc9124050642def4
Matheus Barcellos <mathbar4@hotmail.com>
2023-12-21 08:07:42 -0300

    wip: day 19

---
+++ 2023/python/tests/day19_test.py
@@ -0,0 +1,32 @@
+from ..day19 import parse_input, part1, part2
+
+test_input = """px{a<2006:qkq,m>2090:A,rfg}
+pv{a>1716:R,A}
+lnx{m>1548:A,A}
+rfg{s<537:gd,x>2440:R,A}
+qs{s>3448:A,lnx}
+qkq{x<1416:A,crn}
+crn{x>2662:A,R}
+in{s<1351:px,qqz}
+qqz{s>2770:qs,m<1801:hdj,R}
+gd{a>3333:R,R}
+hdj{m>838:A,pv}
+
+{x=787,m=2655,a=1222,s=2876}
+{x=1679,m=44,a=2067,s=496}
+{x=2036,m=264,a=79,s=2244}
+{x=2461,m=1339,a=466,s=291}
+{x=2127,m=1623,a=2188,s=1013}"""
+
+
+def test_parse():
+    print(parse_input(test_input))
+    assert True
+
+
+def test_part1():
+    assert part1() == 1
+
+
+def test_part2():
+    assert part2() == 1
