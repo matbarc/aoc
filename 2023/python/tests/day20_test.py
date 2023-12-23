d19cad5346a1ec4a0dc2ac5bf20a71052028f029
Matheus Barcellos <mathbar4@hotmail.com>
2023-12-23 08:14:01 -0300

    boilerplate

---
+++ 2023/python/tests/day20_test.py
@@ -0,0 +1,15 @@
+from ..day12 import part1, part2
+
+test_input = """"""
+
+
+def test_parse():
+    assert True
+
+
+def test_part1():
+    assert part1() == 1
+
+
+def test_part2():
+    assert part2() == 1
