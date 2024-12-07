c2cdddf447e7100051774442547bdf97a0e0235f
Matheus Barcellos <mathbar4@hotmail.com>
2024-12-07 09:27:46 -0300

    chore: structure for 2024

---
+++ 2024/python/common/common.py
@@ -0,0 +1,37 @@
+""" This file contains commonly used functions for all problems
+like file reading and the likes
+"""
+
+import pathlib
+from typing import TypeVar, Callable
+
+T = TypeVar("T")
+
+
+def read_file_to_string(fpath: str) -> str:
+    # TODO: Try to get this function to be aware of file entry point (day)
+    # without having to pass it explicitly
+
+    input_fname = fpath.split("/")[-1].replace(".py", ".txt")
+    inputs_path = (pathlib.Path(__file__) / ".." / ".." / ".." / "inputs").resolve()
+
+    with open(inputs_path / input_fname) as fp:
+        file_str = fp.read()
+
+    return file_str
+
+
+def read_file_to_lines(
+    fpath: str, transformation: Callable[[str], T] = lambda x: x
+) -> list[T]:
+    # TODO: Try to get this function to be aware of file entry point (day)
+    # without having to pass it explicitly
+
+    input_fname = fpath.split("/")[-1].replace(".py", ".txt")
+    inputs_path = (pathlib.Path(__file__) / ".." / ".." / ".." / "inputs").resolve()
+    print(inputs_path)
+
+    with open(inputs_path / input_fname) as fp:
+        lines = fp.readlines()
+
+    return [transformation(line) for line in lines]
